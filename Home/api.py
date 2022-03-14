1d61aa7699409bc708fe171c660f5801107559d0
hamza elharchi <hamza.elharchi.elmaslohi@gmail.com>
2022-03-14 09:49:32 +0100

    'c'

--- Home/api.py
+++ Home/api.py
@@ -76,48 +76,8 @@ def taskDelete(request, pk):
 	return Response('Item succsesfully delete!')
 
 
-import base64
 
-import speech_recognition as sr
 
-@api_view(['POST', 'GET'])
-def chatCreate(request):
-	
-	print(type(request.data))
-	
-	
-	
-	r=request.data[22:]
-	
-	r=bytes(r, 'utf-8')
-	wav_file = open("record.wav", "wb")
-	decode_string = base64.b64decode(r)
-	wav_file.write(decode_string)
-	
-	
-
-	# Initialize recognizer class (for recognizing the speech)
-	r = sr.Recognizer()
-
-	# Reading Audio file as source
-	# listening the audio file and store in audio_text variable
-
-	with sr.AudioFile('record.wav') as source:
-		
-		audio_text = r.listen(source)
-		
-	# recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
-		try:
-			
-			# using google speech recognition
-			text = r.recognize_google(audio_text)
-			print('Converting audio transcripts into text ...')
-			print(text)
-		
-		except:
-			print('Sorry.. run again...')
-		
-		return Response('hi hi hi')
 
 
 @api_view(['GET'])
@@ -132,13 +92,32 @@ def chatDetail(request, pk):
 	serializer = ChatSerializer(chat, many=False)
 	return Response(serializer.data)
 
-'''
+
+import base64
+
+import speech_recognition as sr
+
 @api_view(['POST', 'GET'])
 def chatCreate(request):
-	serializer = ChatSerializer(data=request.data)
+	r=request.data[22:]
+	r=bytes(r, 'utf-8')
+	wav_file = open("record.wav", "wb")
+	decode_string = base64.b64decode(r)
+	wav_file.write(decode_string)
+	
+	# Initialize recognizer class (for recognizing the speech)
+	r = sr.Recognizer()
 
-	if serializer.is_valid():
-		serializer.save()
+	# Reading Audio file as source
 
-	return Response(serializer.data)
-	'''
+	with sr.AudioFile('record.wav') as source:
+		audio_text = r.listen(source)
+		
+		try:			
+			text = r.recognize_google(audio_text)
+			print('Converting audio transcripts into text ...')
+			print(text)
+		except:
+			print('Sorry.. run again...')
+		
+		return Response(text)	
